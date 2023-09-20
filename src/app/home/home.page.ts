1859a4c3ab8e39e809e486d21cf3823bc3b301a6
Keidson Roby <keidsondesigner@gmail.com>
2023-09-20 13:02:21 -0400

    feat: :sparkles: implementação tela de login page/home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  showAmount!: boolean;
+	formLogin!: FormGroup;
 
-  constructor() {}
+	constructor(
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.initForm();
+  }
+
+	ngOnInit(): void {
+	}
+
+	toggleShowAmount() {
+		this.showAmount = !this.showAmount;
+		// this.eventChange.emit(this.showAmount);
+	}
+
+	initForm() {
+		this.formLogin = this.formBuilder.group({
+			password: ['', [Validators.required, Validators.minLength(6)]]
+		});
+	}
+
+  // acesso aos controls do formulário
+  get f() { return this.formLogin.controls; }
+
+	onSubmit() {
+		this.router.navigate(['/dashboard']);
+	}
 
 }
