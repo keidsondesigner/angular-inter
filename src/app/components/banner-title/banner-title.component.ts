aa9b5d93b01c49b5b304962c4742b47d86b5812b
Keidson Roby <keidsondesigner@gmail.com>
2024-04-29 20:44:46 -0400

    chore: refatorado component banner-tilte

--- src/app/components/banner-title/banner-title.component.ts
+++ src/app/components/banner-title/banner-title.component.ts
@@ -1,24 +1,19 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-banner-title',
   templateUrl: './banner-title.component.html',
   styleUrls: ['./banner-title.component.scss'],
 })
-export class BannerTitleComponent  implements OnInit {
+export class BannerTitleComponent {
   @Input() bannerTitle: string = '';
 
   @Input() srcBanner: string = '';
 
-  @Output() handleClick: EventEmitter<void> = new EventEmitter<void>();
+  @Output() onClickHandler: EventEmitter<void> = new EventEmitter<void>();
 
-  constructor() { }
-
-  ngOnInit() {}
-
-  emitEvent() {
-    this.handleClick.emit();
-    console.log('Disparando click event filho');
+  emitClickEvent() {
+    this.onClickHandler.emit();
+    console.log('Emitindo event click filho');
   }
-
 }
